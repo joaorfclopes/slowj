582f8d460c4afd28986ac9e93dc7166fed1de727
João Lopes <joaorfclopes@gmail.com>
2019-12-18 20:33:25 +0000

    Missing links in section2 but its already resizable for mobile and desktop

--- src/components/home/Section2.jsx
+++ src/components/home/Section2.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import taosdTitle from "../../assets/images/TAOSD-title.png";
+import taosdCover from "../../assets/images/TAOSD-cover.jpg";
 
 export default class Section2 extends React.Component {
   render() {
     if (this.props.isDesktop === true && this.props.isMobile === false) {
       return (
         <div className="homeSection2">
-          <div className="taosdTitle title">
+          <div className="taosdTitle taosdTitleDesktop title titleDesktop">
             <img
               className="taosdTitleImage taosdTitleImageDesktop"
               src={taosdTitle}
               alt="The Art Of Slowing Down"
             />
+          </div>
+          <div className="taosdCover taosdCoverDesktop cover coverDesktop">
+            <img
+              className="taosdCoverImage taosdCoverImageDesktop"
+              src={taosdCover}
+              alt="The Art Of Slowing Down"
+            />
+          </div>
+          <div className="taosdListen taosdListenDesktop listen listenDesktop">
+            <div className="taosdListenImages taosdListenImagesDesktop"></div>
           </div>
         </div>
       );
@@ -19,12 +30,22 @@ export default class Section2 extends React.Component {
     if (this.props.isDesktop === false && this.props.isMobile === true) {
       return (
         <div className="homeSection2">
-          <div className="taosdTitle title">
+          <div className="taosdTitle taosdTitleMobile title titleMobile">
             <img
               className="taosdTitleImage taosdTitleImageMobile"
               src={taosdTitle}
               alt="The Art Of Slowing Down"
             />
+          </div>
+          <div className="taosdCover taosdCoverMobile cover coverMobile">
+            <img
+              className="taosdCoverImage taosdCoverImageMobile"
+              src={taosdCover}
+              alt="The Art Of Slowing Down"
+            />
+          </div>
+          <div className="taosdListen taosdListenMobile listen listenMobile">
+            <div className="taosdListenImages taosdListenImagesMobile"></div>
           </div>
         </div>
       );
