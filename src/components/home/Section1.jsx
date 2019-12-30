e218a951f71e185ebd32aed0997b63e6e3b7b3de
João Lopes <joaorfclopes@gmail.com>
2019-12-30 15:34:17 +0000

    Entrar button changed

--- src/components/home/Section1.jsx
+++ src/components/home/Section1.jsx
@@ -6,25 +6,42 @@ export default class Section1 extends React.Component {
   scrollToSection2() {
     $("html, body").animate(
       {
-        scrollTop: $(".homeSection2").offset().top - 55
+        scrollTop: $(".homeSection2").offset().top - 10
       },
       500,
       "linear"
     );
   }
   render() {
-    return (
-      <div className="homeSection1">
-        <div className="entrarButtonContainer">
-          <div className="entrarButton" onClick={this.scrollToSection2}>
-            <BasicButton
-              isDesktop={this.props.isDesktop}
-              isMobile={this.props.isMobile}
-              text="Entrar"
-            />
+    if (this.props.isDesktop === true && this.props.isMobile === false) {
+      return (
+        <div className="homeSection1">
+          <div className="entrarButtonContainer entrarButtonContainerDesktop">
+            <div className="entrarButton" onClick={this.scrollToSection2}>
+              <BasicButton
+                isDesktop={this.props.isDesktop}
+                isMobile={this.props.isMobile}
+                text="Entrar"
+              />
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    if (this.props.isDesktop === false && this.props.isMobile === true) {
+      return (
+        <div className="homeSection1">
+          <div className="entrarButtonContainer entrarButtonContainerMobile">
+            <div className="entrarButton" onClick={this.scrollToSection2}>
+              <BasicButton
+                isDesktop={this.props.isDesktop}
+                isMobile={this.props.isMobile}
+                text="Entrar"
+              />
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
