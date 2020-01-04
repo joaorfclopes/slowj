d6561eb39d6efd93f60ef82d370ffeac3f065f22
João Lopes <joaorfclopes@gmail.com>
2020-01-04 17:24:37 +0000

    Section padding changed

--- src/components/home/Section4.jsx
+++ src/components/home/Section4.jsx
@@ -9,25 +9,23 @@ export default class Section4 extends React.Component {
   render() {
     return (
       <div className="homeSection4">
-        <div className="homeSectionContent">
-          <Title
-            isDesktop={this.props.isDesktop}
-            isMobile={this.props.isMobile}
-            title={yafTitle}
-          />
-          <Cover
-            isDesktop={this.props.isDesktop}
-            isMobile={this.props.isMobile}
-            cover={yafCover}
-          />
-          <ListenButtonsSection
-            isDesktop={this.props.isDesktop}
-            isMobile={this.props.isMobile}
-            redirectSpotify="https://open.spotify.com/album/7nmtqSWbVJBxRbpAZT8I6r"
-            redirectAppleMusic="https://music.apple.com/pt/album/the-art-of-slowing-down/1215336873?app=music&ign-mpt=uo%3D4"
-            redirectGooglePlay="https://play.google.com/store/music/album/Slow_J_The_Art_of_Slowing_Down?id=Bs3pnuccst6hzqwg2ccpenivbl4&hl=en"
-          />
-        </div>
+        <Title
+          isDesktop={this.props.isDesktop}
+          isMobile={this.props.isMobile}
+          title={yafTitle}
+        />
+        <Cover
+          isDesktop={this.props.isDesktop}
+          isMobile={this.props.isMobile}
+          cover={yafCover}
+        />
+        <ListenButtonsSection
+          isDesktop={this.props.isDesktop}
+          isMobile={this.props.isMobile}
+          redirectSpotify="https://open.spotify.com/album/7nmtqSWbVJBxRbpAZT8I6r"
+          redirectAppleMusic="https://music.apple.com/pt/album/the-art-of-slowing-down/1215336873?app=music&ign-mpt=uo%3D4"
+          redirectGooglePlay="https://play.google.com/store/music/album/Slow_J_The_Art_of_Slowing_Down?id=Bs3pnuccst6hzqwg2ccpenivbl4&hl=en"
+        />
       </div>
     );
   }
