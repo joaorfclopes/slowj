685f6cfc0337ce49ba00236c57607ae3cab69128
João Lopes <joaorfclopes@gmail.com>
2020-01-06 18:29:05 +0000

    Media item hover effect added

--- src/components/sections/MediaItem.jsx
+++ src/components/sections/MediaItem.jsx
@@ -3,10 +3,9 @@ import React from "react";
 export default class MediaItem extends React.Component {
   render() {
     return (
-      <div
-        className="mediaItem"
-        style={{ backgroundImage: `url(${this.props.image})` }}
-      ></div>
+      <div className="mediaItem">
+        <div className="mediaItemImage" style={{ backgroundImage: `url(${this.props.image})` }}></div>
+      </div>
     );
   }
 }
