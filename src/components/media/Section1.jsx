3d24ca6e8f730aff7823eeb86ecfaab075f88133
João Lopes <joaorfclopes@gmail.com>
2020-01-10 23:01:07 +0000

    MediaItemPage title and subtitle dinamic added for all items

--- src/components/media/Section1.jsx
+++ src/components/media/Section1.jsx
@@ -17,9 +17,122 @@ export default class Section1 extends React.Component {
         title: "Lágrimas (Part 2)",
         subtitle: "Vídeo"
       });
-    } else if (window.location.href.indexOf("publico-you-are-forgiven-critica") > -1) {
+    } else if (
+      window.location.href.indexOf("publico-you-are-forgiven-critica") > -1
+    ) {
       this.setState({
         title: "Público - You Are Forgiven",
+        subtitle: "Crítica"
+      });
+    } else if (
+      window.location.href.indexOf("cca-you-are-forgiven-critica") > -1
+    ) {
+      this.setState({
+        title: "CCA - You Are Forgiven",
+        subtitle: "Crítica"
+      });
+    } else if (window.location.href.indexOf("tambem-sonhar-video") > -1) {
+      this.setState({
+        title: "Também Sonhar (Part 1)",
+        subtitle: "Vídeo"
+      });
+    } else if (
+      window.location.href.indexOf("r&b-you-are-forgiven-critica") > -1
+    ) {
+      this.setState({
+        title: "R&B - You Are Forgiven",
+        subtitle: "Crítica"
+      });
+    } else if (window.location.href.indexOf("r&b-entrevista") > -1) {
+      this.setState({
+        title: "Rimas e Batidas",
+        subtitle: "Entrevista"
+      });
+    } else if (window.location.href.indexOf("nunca-pares-video") > -1) {
+      this.setState({
+        title: "Nunca Pares",
+        subtitle: "Vídeo"
+      });
+    } else if (window.location.href.indexOf("mega-hits-video") > -1) {
+      this.setState({
+        title: "Mega Hits",
+        subtitle: "Vídeo"
+      });
+    } else if (
+      window.location.href.indexOf("r&b-the-art-of-slowing-down-critica") > -1
+    ) {
+      this.setState({
+        title: "R&B - The Art Of Slowing Down",
+        subtitle: "Crítica"
+      });
+    } else if (window.location.href.indexOf("publico-entrevista") > -1) {
+      this.setState({
+        title: "Público",
+        subtitle: "Entrevista"
+      });
+    } else if (window.location.href.indexOf("arte-video") > -1) {
+      this.setState({
+        title: "Arte (Meet Speedy)",
+        subtitle: "Vídeo"
+      });
+    } else if (window.location.href.indexOf("antena-3-video") > -1) {
+      this.setState({
+        title: "Antena 3",
+        subtitle: "Vídeo"
+      });
+    } else if (
+      window.location.href.indexOf("m-de-musica-entrevista-video") > -1
+    ) {
+      this.setState({
+        title: "M de Musica",
+        subtitle: "Entrevista, Vídeo"
+      });
+    } else if (window.location.href.indexOf("vida-boa-video") > -1) {
+      this.setState({
+        title: "Vida Boa",
+        subtitle: "Vídeo"
+      });
+    } else if (
+      window.location.href.indexOf("o-cliente-sofar-lisbon-video") > -1
+    ) {
+      this.setState({
+        title: "O Cliente @Sofar Lisbon",
+        subtitle: "Vídeo"
+      });
+    } else if (window.location.href.indexOf("comida-video") > -1) {
+      this.setState({
+        title: "Comida",
+        subtitle: "Vídeo"
+      });
+    } else if (window.location.href.indexOf("cristalina-video") > -1) {
+      this.setState({
+        title: "Cristalina",
+        subtitle: "Vídeo"
+      });
+    } else if (
+      window.location.href.indexOf("bp-slowj-surge-skate-entrevista-video") > -1
+    ) {
+      this.setState({
+        title: "BP vs Slow J [RVCA | Surge Skate]",
+        subtitle: "Entrevista, Vídeo"
+      });
+    } else if (window.location.href.indexOf("tinta-da-raiz-video") > -1) {
+      this.setState({
+        title: "Tinta da Raiz",
+        subtitle: "Vídeo"
+      });
+    } else if (
+      window.location.href.indexOf(
+        "the-free-food-tape-listening-party-entrevista-video"
+      ) > -1
+    ) {
+      this.setState({
+        title: "The Free Food Tape - Listening Party",
+        subtitle: "Entrevista, Vídeo"
+      });
+    } else if (window.location.href.indexOf("the-free-food-tape-critica") > -1) {
+      this.setState({
+        title: "R&B - The Free Food Tape",
         subtitle: "Crítica"
       });
     }
